e4998bd93c4bf2bf6bfdb10e7545536348fb2407
samiyabatool <samiya.batool@breakthru.pk>
2021-01-21 04:18:18 -0800

    setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Navbar from './navbar';
+import Home from './Home';
 
 function App() {
+  // const title = 'welcomee ';
+  // const likes = 50;
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1>welcome to my blog</h1>
+        <Home/>
+        {/* <h1>{title}</h1>
+        <p>liked {likes}</p> */}
       </div>
     </div>
   );
