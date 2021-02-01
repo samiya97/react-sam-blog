149950c1e60e7f09650dbe5b9b9d6d18144a6175
samiya97 <samiyatirmazi@gmail.com>
2021-02-01 20:58:29 +0500

    usestate hook

--- src/navbar.js
+++ src/navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>Samiya Blog</h1>
             <div className="links">
                 <a href="/">HOME</a>
-                <a href="/create">NEW BLOG</a>
+                <a href="/create" style={{
+                    color:"white",
+                    backgroundColor:' #f1356d',
+                    borderRadius:'8px'
+                }}>NEW BLOG</a>
             </div>
         </nav>
      );
